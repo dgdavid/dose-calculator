23e8eeec4f83bf5d24eed842c8bef9e008b1d9f3
David Díaz <diazglezdavid@gmail.com>
2017-09-20 00:52:14 +0100

    Add basic Home screen

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import { StackNavigator } from 'react-navigation';
 import HomeScreen from './screens/Home';
 
 const DoseCalculator = StackNavigator({
+  Home: { screen: HomeScreen },
 });
 
 export default DoseCalculator;
