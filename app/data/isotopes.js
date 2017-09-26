7ba1c52556214dc12ad40908d264da938e022cda
David Díaz <diazglezdavid@gmail.com>
2017-09-27 00:36:34 +0100

    Initialize isotopes data
    For now, there are only two isotopes.

---
+++ app/data/isotopes.js
@@ -0,0 +1,14 @@
+const isotopes = {
+  'technetium-99m': {
+    name: 'Technetium 99m',
+    halfLife: 6.01,
+    lambda: 0.1153,
+  },
+  'iodine-131': {
+    name: 'Iodine 131',
+    halfLife: 192.48,
+    lambda: 0.0036,
+  },
+};
+
+export default isotopes;
