7c62ad184e70cf3aa8043296bee634f94ecf417f
David Díaz <diazglezdavid@gmail.com>
2017-09-27 00:37:51 +0100

    Add initial Calculator component

---
+++ app/components/Calculator/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    marginTop: 10,
+    padding: 10,
+    paddingBottom: 30,
+  },
+  label: {
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#CCC',
+    marginBottom: 10,
+  },
+});
