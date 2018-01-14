36aa6a184e26bfb38ab9a123b212fee237a56d61
David Díaz <diazglezdavid@gmail.com>
2018-01-14 01:05:44 +0100

    Update IsotopePicker field

--- app/components/fields/IsotopePicker/IsotopePicker.js
+++ app/components/fields/IsotopePicker/IsotopePicker.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 
 import isotopes from '../../../data/isotopes';
 
-function IsotopePicker(props) {
-  return (
-    <Picker {...props}>
-      {
-        Object.keys(props.isotopes).map((key) =>
-          <Picker.Item label={props.isotopes[key].name} value={key} key={key} />)
-      }
+const IsotopePickerOptions = Object.keys(isotopes).map((key) => (
+  <Picker.Item
+    label={isotopes[key].name}
+    key={key}
+    value={key}
+  />
+));
+
+const IsotopePicker = ({ prompt, ...props }) => (
+  <Fragment>
+    <Text style={styles.prompt}>{prompt}</Text>
+    <Picker
+      style={styles.picker}
+      {...props}
+    >
+      {IsotopePickerOptions}
     </Picker>
-  );
-}
+  </Fragment>
+);
 
 IsotopePicker.propTypes = {
-  isotopes: PropTypes.object,
+  prompt: PropTypes.string,
 };
 
 IsotopePicker.defaultProps = {
-  isotopes,
+  prompt: 'Select isotope',
+};
+
+const colors = {
+  dimGray: '#6A6A6A',
+};
+
+const styles = {
+  prompt: {
+    color: colors.dimGray,
+    fontSize: 30,
+    paddingTop: 5,
+    alignSelf: 'center',
+  },
+
+  picker: {
+    position: 'absolute',
+    top: 0,
+    width: 1000,
+    height: 1000,
+  },
 };
 
 export default IsotopePicker;
