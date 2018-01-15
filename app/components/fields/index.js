36aa77102d804a9fddf6f9c778e7e029404d316d
David Díaz <diazglezdavid@gmail.com>
2018-01-15 21:53:01 +0100

    Expose custom inputs directly from a/c/fields/

---
+++ app/components/fields/index.js
@@ -0,0 +1,9 @@
+import DateTimeInput from './DateTimeInput';
+import IsotopePicker from './IsotopePicker';
+import NumericInput from './NumericInput';
+
+export {
+  DateTimeInput,
+  IsotopePicker,
+  NumericInput,
+};
