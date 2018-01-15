b03b3697a68714c074f30d735c6ce0ad0e738bab
David Díaz <diazglezdavid@gmail.com>
2018-01-16 00:29:52 +0100

    Fix IsotopePicker fields
    In previous commit was forgotten to add a property in a wrapper in order
    to get Picker clipped to its edges.
    
    This commit add the property used in the source of "trick", but
    `backgroundColor: 'transparent'` also make it works.
    
    See original idea at https://github.com/facebook/react-native/issues/7817#issuecomment-264851951

--- app/components/fields/IsotopePicker/IsotopePicker.js
+++ app/components/fields/IsotopePicker/IsotopePicker.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Picker, Text } from 'react-native';
+import { Picker, Text, View } from 'react-native';
 
 import isotopes from '../../../data/isotopes';
 import colors from '../../../config/colors';
@@ -15,13 +15,15 @@ const IsotopePickerOptions = Object.keys(isotopes).map((key) => (
 
 const IsotopePicker = ({ prompt, ...props }) => (
   <Fragment>
-    <Text style={styles.prompt}>{prompt}</Text>
-    <Picker
-      style={styles.picker}
-      {...props}
-    >
-      {IsotopePickerOptions}
-    </Picker>
+    <View style={styles.wrapper}>
+      <Text style={styles.prompt}>{prompt}</Text>
+      <Picker
+        style={styles.picker}
+        {...props}
+      >
+        {IsotopePickerOptions}
+      </Picker>
+    </View>
   </Fragment>
 );
 
@@ -34,6 +36,12 @@ IsotopePicker.defaultProps = {
 };
 
 const styles = {
+  wrapper: {
+    // Fix to get Picker clipped at the edges of wrapper and make the "prompt trick" works.
+    // See https://github.com/facebook/react-native/issues/7817#issuecomment-264851951 to know more.
+    borderWidth: 0,
+  },
+
   prompt: {
     color: colors.dimGray,
     fontSize: 30,
