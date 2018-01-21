096a841cbbff708a0afe35c0399e483ce9bc05ff
David Díaz <diazglezdavid@gmail.com>
2018-01-21 22:23:31 +0100

    Fix, again, the IsotopePicker
    Due a bugs with React Native Picker component. Read the comment left in
    the file for more info.

---
+++ app/data/__mocks__/isotopes.js
@@ -0,0 +1,15 @@
+
+const isotopes = {
+  i1: {
+    name: 'Isotope 1',
+    halfLife: 192.48,
+    lambda: 0.0036,
+  },
+  i2: {
+    name: 'Isotope 2',
+    halfLife: 6.01,
+    lambda: 0.1153,
+  },
+};
+
+export default isotopes;
