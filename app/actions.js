5ff2fffa4dea2dde2e34858a71795e206efb7002
David Díaz <diazglezdavid@gmail.com>
2018-01-31 00:20:00 +0100

    Move from internal state to props (connected component)

--- app/actions.js
+++ app/actions.js
@@ -1,6 +1,8 @@
 import calculateVolume from './lib/calculators/volume';
 
 export default actions = store => ({ // eslint-disable-line
+  // FIXME: action should be as pure as possible
+  // TODO: add tests
   calculateVolume: (state, data) => {
     const result = calculateVolume({ ...state, ...data });
 
--- app/screens/VolumeCalculator.test.js
+++ app/screens/VolumeCalculator.test.js
@@ -2,7 +2,7 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import VolumeCalculator from './VolumeCalculator';
+import { VolumeCalculator } from './VolumeCalculator';
 
 jest.mock('../components/fields/DateTimeInput');
 
